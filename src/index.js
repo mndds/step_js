603c0a10792c0b47039d860de52ff64d0489ef9f
mndds <73651166+mndds@users.noreply.github.com>
2021-10-14 22:49:15 +0600

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import '../assets/style.scss';
+
+const axios = require('axios'); 
+
+const cat = document.getElementById('cat-photo');
+
+axios.get('https://aws.random.cat/meow')
+    .then(response => {
+        const data = response.data;
+        cat.src = data.file;
+        cat.style.width = 600+'px';        
+    });

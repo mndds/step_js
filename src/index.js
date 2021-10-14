f096e29d20c0ad1f87b62fe55e1829cba0f8c5f6
mndds <73651166+mndds@users.noreply.github.com>
2021-10-15 00:20:01 +0600

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import '../assets/style.scss';
 
-const axios = require('axios'); 
+    const axios = require('axios'); 
+    const cat = document.getElementById('cat-photo');
+    const btn = document.getElementById('btn-update');
 
-const cat = document.getElementById('cat-photo');
+    function updatePhoto() {
+        axios.get('https://aws.random.cat/meow')
+            .then(response => {
+                const data = response.data;
+                cat.src = data.file;                    
+            });
+    }
 
-axios.get('https://aws.random.cat/meow')
-    .then(response => {
-        const data = response.data;
-        cat.src = data.file;
-        cat.style.width = 600+'px';        
-    });
+window.addEventListener('load', () => {    
+    updatePhoto();   
+});
+
+btn.addEventListener('click', () => {
+    updatePhoto();
+});
+
+    
